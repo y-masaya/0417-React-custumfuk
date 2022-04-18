ee1e57545220420b0486100e7a81dcd7cd375935
y-masaya <49806793+y-masaya@users.noreply.github.com>
2022-04-18 23:46:19 +0900

    Initial commit

---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState } from "react";
+import { UserProfile } from "../types/userProfile";
+import { User } from "../types/api/user";
+
+export const useAllUsers = () => {
+  const [userProfile, setUserProfile] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getUsers = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfile(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  return { getUsers, userProfile, loading, error };
+};
